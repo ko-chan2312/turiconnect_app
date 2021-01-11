d339762fec0194ec4b23a8dc6929a450beef010b
kohei <ymca0624plane@yahoo.co.jp>
2021-01-11 13:51:58 +0900

    Add docker introductions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "サンプルユーザー",
+    email: "sampleuser@gmail.com",
+    password:             "tripsample",
+    password_confirmation: "tripsample")
